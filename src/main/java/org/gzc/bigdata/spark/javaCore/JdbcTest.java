00ad72f7ef451d51b6396062f181a51cf496e4ff
guozc <guozc@bsfit.com.cn>
2023-07-28 17:17:43 +0800

    feat: spark jdbc 学习

---
+++ src/main/java/org/gzc/bigdata/spark/javaCore/JdbcTest.java
@@ -0,0 +1,30 @@
+package org.gzc.bigdata.spark.javaCore;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.List;
+import java.util.Properties;
+
+
+public class JdbcTest {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("MyApp")
+                .master("local")
+                .getOrCreate();
+        String url = "jdbc:mysql://localhost:12306/bsm";
+        String table = "user";
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "GZCabc123");
+        properties.setProperty("driver", "com.mysql.cj.jdbc.Driver");
+
+        Dataset<Row> jdbc = spark.read().jdbc(url, table, properties);
+        List<Row> rows = jdbc.collectAsList();
+        for (Row row : rows) {
+            System.out.println(row.json());
+        }
+    }
+}
